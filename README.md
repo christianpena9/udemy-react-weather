6a8c53ace274d850e5d71d4aa6ee9298d31cc002
Christian Pena <christianp@tarte.com>
2019-01-01 20:19:43 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# udemy-hello-react
---
+++ app/app.jsx
@@ -0,0 +1,10 @@
+/**
+ * React Component. Good practice to use uppercase on the react component
+ * You can only return one element, error will be thrown if multiple root
+ * html elements are returned
+ */
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(<h1>Boilerplate app!</h1>, document.getElementById('app'));
