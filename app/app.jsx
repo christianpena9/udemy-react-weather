47b43254202a7353161a03524a8aa717da81fda3
Christian Pena <christianp@tarte.com>
2019-01-01 21:47:50 -0500

    updated react router to latest version. also created a main component to render

--- app/app.jsx
+++ app/app.jsx
@@ -1,10 +1,11 @@
-/**
- * React Component. Good practice to use uppercase on the react component
- * You can only return one element, error will be thrown if multiple root
- * html elements are returned
- */
-
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory, browserHistory} = require('react-router');
+var Main = require('Main');
 
-ReactDOM.render(<h1>Boilerplate app!</h1>, document.getElementById('app'));
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path="/" component={Main}></Route>
+    </Router>,
+    document.getElementById('app')
+);
