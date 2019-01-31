6610e54ba66910750351a046f33a28fe9364af1d
Christian Pena <christianp@tarte.com>
2019-01-30 23:09:29 -0500

    refactored middleware for localhost

--- server.js
+++ server.js
@@ -6,14 +6,10 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'http') {
-        next();
+    if (req.headers['x-forwarded-proto'] === 'https') {
+        res.redirect('http://' + req.hostname + req.url);
     } else {
-        if (PORT !== 3000) {
-            res.redirect('http://' + req.hostname + req.url);
-        } else {
-            next();
-        }
+        next();
     }
 })
 
