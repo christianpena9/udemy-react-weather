bb3fd2cedf2ad907be9fdac856159ac1d1df77af
Christian Pena <christianp@tarte.com>
2019-01-30 22:59:25 -0500

    updated weathermessage component since it had an error

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@
 var React = require('react');
 
 var WeatherMessage = ({temp, location}) => { // new way for destructuring
-    var {temp, location} = props; // another way for destructuring
+    //var {temp, location} = props; // another way for destructuring
     return (
         <p>It's {temp} in {location}.</p>
     );
