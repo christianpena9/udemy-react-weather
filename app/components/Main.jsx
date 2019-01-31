b688f5620cfb16b7c1a4fb141a7b3b9a74326fd3
Christian Pena <christianp@tarte.com>
2019-01-30 21:10:39 -0500

    refactored the components to be simple and stateless

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -3,16 +3,14 @@ var React = require('react');
 /* Custom Components */
 var Nav = require('Nav');
 
-var Main = React.createClass({
-    render: function () {
-        return (
-            <div>
-                <Nav />
-                <h2>Main Component</h2>
-                {this.props.children}
-            </div>
-        );
-    }
-});
+var Main = (props) => {
+    return (
+        <div>
+            <Nav />
+            <h2>Main Component</h2>
+            {props.children}
+        </div>
+    );
+};
 
 module.exports = Main;
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,15 +1,11 @@
 
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-    render: function() {
-        // destructuring
-        var {temp, location} = this.props;
-
-        return (
-            <p>It's {temp} in {location}.</p>
-        );
-    }
-});
+var WeatherMessage = ({temp, location}) => { // new way for destructuring
+    var {temp, location} = props; // another way for destructuring
+    return (
+        <p>It's {temp} in {location}.</p>
+    );
+};
 
 module.exports = WeatherMessage;
